e35c0b87937fc840c159b316340b118614f151c5
raycashmore <ray@raycashmore.com>
2013-04-03 23:09:36 +1100

    Info panels, modals

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -5,16 +5,16 @@ angular.module('rrDirectives', [])
     /**
      * Window resize
      */
-    .directive('resize', ["$window", function ($window) {
+    .directive('resizeBanner', ["$window", function ($window) {
         return function (scope) {
             scope.width = $window.innerWidth;
             scope.height = $window.innerHeight;
-            scope.heightStyle = 'height:' + ($window.innerHeight - 112) + 'px';
+            scope.heightStyle = 'height:' + ($window.innerHeight - 125) + 'px';
             angular.element($window).bind('resize', function () {
                 scope.$apply(function () {
                     scope.width = $window.innerWidth;
                     scope.height = $window.innerHeight;
-                    scope.heightStyle = 'height:' + ($window.innerHeight - 112) + 'px';
+                    scope.heightStyle = 'height:' + ($window.innerHeight - 125) + 'px';
                 });
             });
         };
